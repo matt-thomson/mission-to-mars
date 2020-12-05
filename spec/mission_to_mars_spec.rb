ed022d52a368dd799dbf05d0895588aa0aabb028
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-05 14:09:05 +0000

    Correct Rubocop offenses

--- spec/mission_to_mars_spec.rb
+++ spec/mission_to_mars_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe MissionToMars do
-  it "has a version number" do
+  it 'has a version number' do
     expect(MissionToMars::VERSION).not_to be nil
   end
 end
