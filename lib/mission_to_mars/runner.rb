f45989090861de7c722aef187e528f7489fd3153
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-05 14:29:58 +0000

    Parse planet from file in runner
    This adds some temporary output so that we can assert that the parsing
    is working properly.

--- lib/mission_to_mars/runner.rb
+++ lib/mission_to_mars/runner.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
+require_relative 'planet'
+
 module MissionToMars
   # Entry point to the application.
   class Runner
-    def initialize(output: $stdout)
+    def initialize(path:, output: $stdout)
+      @path = path
       @output = output
     end
 
     def run
-      @output.puts 'hello, world'
+      File.open(@path) do |file|
+        planet = Planet.parse(file.each_line.first)
+
+        @output.puts "width: #{planet.width}, height: #{planet.height}"
+      end
     end
   end
 end
