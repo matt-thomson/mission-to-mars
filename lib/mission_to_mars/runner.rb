3edf472f326caa1fec3a5e26485cd66682f7c7e5
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-07 19:40:34 +0000

    Lazily read lines from input
    Previously, the runner would attempt to load the entire input into
    memory before processing any of it.  For very large inputs, this could
    lead to very high memory usage.  This is shown by running the following,
    which generates an arbitrarily long input.  The memory used by the
    process grows linearly and eventually runs out.
    
    { echo '5 3' & yes '1 1 E\nRFRFRFRF'; } | bin/mars
    
    This commit changes the runner to lazily read lines from the input,
    processing each line as it is read.  With this change, the memory usage
    remains bounded.
    
    Since the co-ordinates and instruction strings are bounded (at 50 and
    100 respectively), this means that any input can be handled in bounded
    memory.

--- lib/mission_to_mars/runner.rb
+++ lib/mission_to_mars/runner.rb
@@ -14,12 +14,14 @@ module MissionToMars
     end
 
     def run
-      planet = Planet.parse(@input.each_line.first)
+      planet = Planet.parse(input_lines.first)
 
-      @input.each_line.map(&:strip).reject(&:empty?).each_slice(2) do |robot_line, instructions_line|
+      input_lines.each_slice(2) do |robot_line, instructions_line|
         deploy(planet, robot_line, instructions_line)
       end
     end
+
+    private
 
     def deploy(planet, robot_line, instructions_line)
       robot = Robot.parse(robot_line)
@@ -29,6 +31,10 @@ module MissionToMars
       deployment.run!
 
       @output.puts(deployment)
+    end
+
+    def input_lines
+      @input.each_line.lazy.map(&:strip).reject(&:empty?)
     end
   end
 end
