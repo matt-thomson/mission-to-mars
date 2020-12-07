812782fa3724fd3a6a1f85faabf93f25740eee12
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-07 08:30:32 +0000

    Extract move method to individual compass points

--- lib/mission_to_mars/compass_point.rb
+++ lib/mission_to_mars/compass_point.rb
@@ -6,10 +6,29 @@ module MissionToMars
   # CompassPoint represents one of the four primary compass directions.
   class CompassPoint < TypesafeEnum::Base
     # These are in clockwise order.
-    new :NORTH, 'N'
-    new :EAST, 'E'
-    new :SOUTH, 'S'
-    new :WEST, 'W'
+    new :NORTH, 'N' do
+      def move(x, y)
+        [x, y + 1]
+      end
+    end
+
+    new :EAST, 'E' do
+      def move(x, y)
+        [x + 1, y]
+      end
+    end
+
+    new :SOUTH, 'S' do
+      def move(x, y)
+        [x, y - 1]
+      end
+    end
+
+    new :WEST, 'W' do
+      def move(x, y)
+        [x - 1, y]
+      end
+    end
 
     def anticlockwise
       CompassPoint.find_by_ord((ord - 1) % 4)
@@ -17,15 +36,6 @@ module MissionToMars
 
     def clockwise
       CompassPoint.find_by_ord((ord + 1) % 4)
-    end
-
-    def move(x, y)
-      case self
-      when NORTH then [x, y + 1]
-      when EAST then [x + 1, y]
-      when SOUTH then [x, y - 1]
-      when WEST then [x - 1, y]
-      end
     end
   end
 end
