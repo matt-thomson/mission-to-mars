0c8ce5ac1b3e8ff1d569a5e8bb8144256223e06f
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-07 08:29:03 +0000

    Remove lookup for compass point rotations
    We can calculate the next compass point in any direction instead of
    having a lookup table.

--- lib/mission_to_mars/compass_point.rb
+++ lib/mission_to_mars/compass_point.rb
@@ -5,27 +5,18 @@ require 'typesafe_enum'
 module MissionToMars
   # CompassPoint represents one of the four primary compass directions.
   class CompassPoint < TypesafeEnum::Base
+    # These are in clockwise order.
     new :NORTH, 'N'
     new :EAST, 'E'
     new :SOUTH, 'S'
     new :WEST, 'W'
 
     def anticlockwise
-      case self
-      when NORTH then WEST
-      when EAST then NORTH
-      when SOUTH then EAST
-      when WEST then SOUTH
-      end
+      CompassPoint.find_by_ord((ord - 1) % 4)
     end
 
     def clockwise
-      case self
-      when NORTH then EAST
-      when EAST then SOUTH
-      when SOUTH then WEST
-      when WEST then NORTH
-      end
+      CompassPoint.find_by_ord((ord + 1) % 4)
     end
 
     def move(x, y)
